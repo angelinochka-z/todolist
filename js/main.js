a570ed3282a10e367d5c63f9a54addd469ca2f92
angelinochka-z <bajbulatovaangelina@gmail.com>
2025-03-21 16:48:59 +0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,130 @@
+// элементы на странице
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
+const emptyList = document.querySelector('#emptyList');
+
+let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach((task) => renderTask(task));
+}
+
+checkEmptyList();
+
+form.addEventListener('submit', addTask);
+tasksList.addEventListener('click', deleteTask);
+tasksList.addEventListener('click', doneTask);
+
+// функции
+function addTask(event) {
+    //отмена отправки формы
+    event.preventDefault();
+
+    // доставка текста задачи из поля ввода
+    const taskText = taskInput.value
+
+    // описывание задачи в виде объекта
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false,
+    };
+    //добавление задачи в массив с задачами
+    tasks.push(newTask);
+
+    //сохранение списка задач в хранилище браузера localstorage
+    saveToLocalStorage();
+    // рендер задачи на странице
+    renderTask(newTask);
+  
+    //очищение поле ввода и возвращение на него внимание
+    taskInput.value = '';
+    taskInput.focus();
+
+    checkEmptyList();
+}
+
+function deleteTask(event) { 
+    // проверка, что клик был НЕ по кнопке "удалить задачу" 
+    if (event.target.dataset.action !== 'delete') return;
+
+    // проверка, что клик был на кнопке "удалить задачу"
+    const parenNode = event.target.closest('.list-group-item');
+
+    // определяем ID задачу
+    const id = Number(parenNode.id);
+
+    // удаляем задачу через фильтрацию массива
+    tasks = tasks.filter((task) => task.id !== id);
+
+    //сохраняем список задач в хранилище браузера localstorage
+    saveToLocalStorage();
+
+    // удаляем задачу из разметки
+    parenNode.remove();
+
+    checkEmptyList();
+}
+
+function doneTask(event) {
+   // проверка, что клик был НЕ по кнопке "задача выпонена"
+   if (event.target.dataset.action !== 'done') return; 
+
+    // проверка, что клик был по кнопке "задача выпонена"
+    
+    const parentNode = event.target.closest('.list-group-item');
+
+    //определение ID задачи
+    const id = Number(parentNode.id);
+    const task = tasks.find( (task) => task.id === id);
+    task.done = !task.done;
+
+    //сохранеие списка задач в хранилище браузера localstorage
+    saveToLocalStorage();
+
+    const taskTitle = parentNode.querySelector('.task-title');
+    taskTitle.classList.toggle('task-title--done');
+}
+
+function checkEmptyList() {
+    if (tasks.length === 0) {
+        const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+					<img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+					<div class="empty-list__title">Список дел пуст</div>
+                    </li>`
+        tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+}
+
+    if (tasks.length > 0) {
+        const emptyListEl = document.querySelector('#emptyList');
+        emptyListEl ? emptyListEl.remove() : null;
+    }
+}
+
+function saveToLocalStorage() {    
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTask(task) {
+    // формирование CSS класс
+    const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+    
+    // разметка для новой задачи
+    const taskHTML = `
+                    <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+                        <span class="${cssClass}">${task.text}</span>
+                        <div class="task-item__buttons">
+                            <button type="button" data-action="done" class="btn-action">
+                                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                            </button>
+                            <button type="button" data-action="delete" class="btn-action">
+                                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                            </button>
+                        </div>
+                    </li>`;
+    
+    // добавление задачу на страницу
+    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+}
